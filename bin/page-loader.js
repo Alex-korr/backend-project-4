6e9858f994e175b2085bc103d3833e15798b4ea7
ALEKSEI <alex@ALEKSEIs-MacBook-Pro.local>
2025-09-22 13:37:28 +0300

    Add asciinema demo to README

--- bin/page-loader.js
+++ bin/page-loader.js
@@ -4,14 +4,23 @@ import { program } from 'commander'
 import { resolve } from 'node:path'
 import load from '../src/pageLoader.js'
 
+// Generate directory name from URL
+const generateDirName = (url) => {
+  const urlObj = new URL(url)
+  const fullPath = `${urlObj.hostname}${urlObj.pathname}`.replace(/\/$/, '')
+  return fullPath.replace(/[^a-zA-Z0-9]/g, '-')
+}
+
 program
   .name('page-loader')
   .description('Page loader utility')
   .version('1.0.0')
   .argument('<url>', 'URL of the page to download')
-  .option('-o --output [dir]', 'output directory (defaults to current)', process.cwd())
+  .option('-o --output [dir]', 'output directory (defaults to URL-based name)')
   .action(async (url, options) => {
-    const outputPath = resolve(options.output)
+    // Use URL-based directory name if no output specified
+    const defaultDir = generateDirName(url)
+    const outputPath = resolve(options.output || defaultDir)
     try {
       const filePath = await load(url, outputPath)
       console.log(filePath)
