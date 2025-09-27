320a0662f00bce237e4a03b81ced5718d8de3693
ALEKSEI <alex@ALEKSEIs-MacBook-Pro.local>
2025-09-27 14:44:31 +0300

    Remove window reference from runtime.js to pass Hexlet CI lint

--- __tests__/__fixtures__/runtime.js
+++ __tests__/__fixtures__/runtime.js
@@ -1,13 +1,10 @@
-/* eslint-env browser */
 console.log('Runtime script loaded from Hexlet!')
 
 // Some runtime initialization
 
-if (typeof window !== 'undefined') {
-  window.HexletRuntime = {
-    version: '1.0.0',
-    init: function () {
-      console.log('Hexlet runtime initialized')
-    },
-  }
+const HexletRuntime = {
+  version: '1.0.0',
+  init: function () {
+    console.log('Hexlet runtime initialized')
+  },
 }
